1a665f2c7f49582a04b9f7c4139f609c5d8c8489
Roman Grishin <roman.grishin.me@gmail.com>
2022-07-01 20:20:23 +0300

    pp fixes, ourworks hover, fonts fixes, tag's selectors removed

--- script/script.js
+++ script/script.js
@@ -1,36 +1,17 @@
 " use strict";
 
-let navButton = document.getElementsByClassName('nav__button');
-    nav = document.getElementsByClassName('nav__menu');
-    close = document.getElementsByClassName('close');
-
-let galleryPhotoCardArr = [
-        './img/jpeg/P1.jpg',
-        './img/jpeg/P2.jpg',
-        './img/jpeg/P3.jpg',
-        './img/jpeg/P4.jpg',
-        './img/jpeg/P5.jpg',
-        './img/jpeg/P6.jpg'
-];
-
-galleryCard = document.getElementsByClassName('ourworks__gallery__item');
-
-console.log(navButton);
-console.log(nav);
-console.log(galleryCard);
-
+let navButton = document.getElementsByClassName('nav__button'),
+    nav = document.getElementsByClassName('nav__menu'),
+    close = document.getElementsByClassName('close'),
+    bodyScrollHidden = document.getElementsByTagName('html');
 
 
 navButton[0].addEventListener('click', function() {
     nav[0].classList.add('active');
+    bodyScrollHidden[0].classList.add('scroll__hidden');
 });
 
 close[0].addEventListener('click', function() {
     nav[0].classList.remove('active');
+    bodyScrollHidden[0].classList.remove('scroll__hidden');
 });
-
-function addBackgroundGallery() {
-    for (i = 0; i < galleryPhotoCardArr.length; i++) {
-        galleryCard[i].style.backgroundColor = green;
-    }
-}
